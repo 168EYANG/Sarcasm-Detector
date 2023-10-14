a62cea6a1291ed343a6215e46854a174664df8ad
mitchteun <mitchteun@gmail.com>
2023-10-14 03:14:21 -0400

    drawing board on website

--- hackgtx-site/mern/client/src/components/home.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Home() {
-    return (
-        <div class="bg-primary d-flex justify-content-center align-items-center" style={{ height: 100+'vh' }}>
-            <h1 style={{ color: "#ffffff" }}>Customon</h1>
-        </div>
-    )
-}
