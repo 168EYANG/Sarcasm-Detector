cbbc832184821c03254e9162c8b6c89adda57249
mitchteun <mitchteun@gmail.com>
2023-10-14 03:05:59 -0400

    Minimum possible website

--- hackgtx-site/mern/client/src/App.js
+++ hackgtx-site/mern/client/src/App.js
@@ -1,25 +1,9 @@
 import React from "react";
 
-// use Route to define different routes of the app
-import { Route, Routes } from "react-router-dom";
-
-// import all the components
-import Navbar from "./components/navbar";
-import Home from "./components/home";
-import RecordList from "./components/recordList";
-import Edit from "./components/edit";
-import Create from "./components/create";
-
 const App = () => {
     return (
         <div>
-            <Navbar />
-            <Routes>
-                <Route exact path="/" element={<Home />}/>
-                <Route path="/records" element={<RecordList />} />
-                <Route path="/edit/:id" element={<Edit />} />
-                <Route path="/create" element={<Create />} />
-            </Routes>
+            
         </div>
     );
 };
